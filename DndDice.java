be2842f5a5a50266ed3efb222a097f46b21f8767
josegutierrez420 <59151056+josegutierrez420@users.noreply.github.com>
2020-01-08 23:19:37 -0800

    Add files via upload
    Attempted to fix issue #1, ended up creating fail-safe for when anything other than an int is entered for 'sides' and 'times'

--- DndDice.java
+++ DndDice.java
@@ -4,29 +4,37 @@ public class DndDice
 {
     public static void main(final String[] args)
     {
-        final Scanner console = new Scanner(System.in);
+        Scanner console = new Scanner(System.in);
         System.out.println("What sided dice do you want to roll?");
-        final int sides = console.nextInt();
-        if(sides > 20)
+        while(!console.hasNextInt())                                //add event to rerun process if > 20
         {
-            System.out.println("Amount is too high, choose a different amount.");
+            System.out.println("Invalid, please enter a number.");
+            console.next();
         }
+        while(console.nextInt() < 20)                                
+        {
+            System.out.println("Value too large, please enter a number below or equal to 20.");
+            console.next();
+
+        }
+        int sides = console.nextInt();
         System.out.println("How many times do you want to roll?");
-        final int times = console.nextInt();
-        if(times > 20)
+        while(!console.hasNextLine())//add event to rerun process if > 20
         {
-            System.out.println("Amount is too high, choose a different amount.");
+            System.out.println("Invalid, please enter a number.");
+            console.next();
         }
+        int times = console.nextInt();
         Method(times, sides);
         console.close();
     }
     public static void Method(final int x, final int y)
     {
-        final Random r = new Random();
+        Random r = new Random();
         for(int i = 0; i < x; i++)
         {
-            final int result = r.nextInt(y) + 1;
-            System.out.println("d" + y + ": " + result);
+            int result = r.nextInt(y) + 1;
+            System.out.println("Roll #" + i + ", d" + y + ": " + result);
         }
     }
 }
