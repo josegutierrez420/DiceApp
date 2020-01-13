97cd9a3ef9010bfd3fd7462b6162fe4daeaa597c
josegutierrez420 <59151056+josegutierrez420@users.noreply.github.com>
2020-01-12 16:37:24 -0800

    Add files via upload
    -Cleaned the code
    -Added comments

--- DndDice.java
+++ DndDice.java
@@ -2,33 +2,46 @@ import java.util.*;
 
 public class DndDice
 {
-    public static void main(final String[] args)
+    public static void main(String[] args)
     {
+        //declaring and initializing variables
         Scanner console = new Scanner(System.in);
-        System.out.println("What sided dice do you want to roll?");
-        while(!console.hasNextInt())                                //add event to rerun process if > 20
-        {
-            System.out.println("Invalid, please enter a number.");
-            console.next();
-        }
-        int sides = console.nextInt();
-        System.out.println("How many times do you want to roll?");
-        while(!console.hasNextLine())//add event to rerun process if > 20
-        {
-            System.out.println("Invalid, please enter a number.");
-            console.next();
-        }
-        int times = console.nextInt();
-        Method(times, sides);
-        console.close();
-    }
-    public static void Method(final int x, final int y)
-    {
         Random r = new Random();
-        for(int i = 0; i < x; i++)
+        int sides = 0, times = 0;
+        //call methods
+        sides = DieSides(console, sides);
+        times = DieRoll(console, times);
+        DieResult(sides, times, r);
+    }
+    //Asking user what type of die to use
+    public static int DieSides(Scanner a, int x)
+    {
+        System.out.println("What sided dice do you want to roll?");
+        while(!a.hasNextInt())
         {
-            int result = r.nextInt(y) + 1;
-            System.out.println("Roll #" + (i + 1) + ", d" + y + ": " + result);
+            System.out.println("Please type in a numeric value");
+        }
+        x = a.nextInt();
+        return x;
+    }
+    //Asking user how many times they want to roll the die/dice
+    public static int DieRoll(Scanner b, int y)
+    {
+        System.out.println("How many times do you want to roll?");
+        while(!b.hasNextInt())
+        {
+            System.out.println("Please type in a numeric value");
+        }
+        y = b.nextInt();
+        return y;
+    }
+    //Returning roll results
+    public static void DieResult(int w, int z, Random r1)
+    {
+        for(int i = 0; i > z; i++)
+        {
+            int result = r1.nextInt(z) + 1;
+            System.out.println("Roll #" + (i + 1) + ", d" + w + ": " + result);
         }
     }
 }
